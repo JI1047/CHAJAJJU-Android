210ee4ecfecd15b74cf85fb1f01466a2b97ad113
김지석 <kjs001004@naver.com>
2025-11-22 16:42:02 +0900

    Update home.tsx

--- app/footer/home.tsx
+++ app/footer/home.tsx
@@ -110,6 +110,8 @@ export default function HomeScreen() {
 
               // 지도 중심 이동
               map.setCenter(moveLatLng);
+              map.setLevel(3);
+
 
               // 🔥 기존 마커 클릭 유지됨! (검색 마커 생성 X)
               if (currentInfoWindow) {
